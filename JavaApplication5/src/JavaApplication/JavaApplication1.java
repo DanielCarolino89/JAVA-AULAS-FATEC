c468cb1c84007e9ff09f8eb9cb6a05352792c93f
Daniel de Godoy Carolino <99697178+DanielCarolino89@users.noreply.github.com>
2022-09-02 19:45:39 -0300

    Add files via upload

---
+++ JavaApplication5/src/JavaApplication/JavaApplication1.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno.si
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //entrada de dados
+        Scanner sc = new Scanner(System.in);
+        
+        int x=5; // vari�vel do tipo int
+        double d=2.5 ;
+        String s="texto" ;
+       
+        System.out.println(" valor inteiro "+x);
+        System.out.println(" valor inteiro "+d);
+        System.out.println(" valor inteiro "+s);
+    }
+}
+
